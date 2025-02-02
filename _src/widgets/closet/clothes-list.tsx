7fb9a9b424125a8681b22f26659ab002bf946dc2
R1013-T <takahashi.ryunosuke.job@gmail.com>
2025-02-02 18:33:35 +0900

    🚀

--- _src/widgets/closet/clothes-list.tsx
+++ _src/widgets/closet/clothes-list.tsx
@@ -4,7 +4,7 @@ export default function ClothesList() {
   return (
     <div className="grid grid-cols-3 md:grid-cols-4">
       {
-        Array.from({ length: 15 }).map((_, index) => (
+        Array.from({ length: 30 }).map((_, index) => (
           <ClothesItem key={index} id={String(index)} />
         ))
       }
