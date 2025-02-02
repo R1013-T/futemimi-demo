e3d73688b85421a77d3efc5e55f19e1336d5b283
R1013-T <takahashi.ryunosuke.job@gmail.com>
2025-02-02 18:17:02 +0900

    🚀

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,15 +1,14 @@
-import PageLayout from '~/shared/layout/page-layout'
-import NavBar from '~/shared/ui/nav-bar'
+import NavBar from "~/shared/ui/nav-bar";
 
 export default function MainLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <div>
       {children}
       <NavBar />
     </div>
-  )
+  );
 }
